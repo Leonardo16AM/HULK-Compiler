c1e857021cb377655960714717190e0129e94b4e
Leonardo16AM <leo16am@gmail.com>
2024-06-03 23:54:39 -0400

    feat: :page_facing_up: Added regex classes

---
+++ src/regex/regex_grammar.h
@@ -0,0 +1,14 @@
+#ifndef REGEX_GRAMMAR_H
+#define REGEX_GRAMMAR_H
+
+#include <string>
+#include <vector>
+
+
+
+
+std::vector<char> regex_special_tokens= {'?', '+', '*', '^',
+                        '$', '[', ']', '(', ')', '{', '}', '.', '|', '-'};
+
+
+#endif
---
+++ src/tests/regex_test.h
@@ -0,0 +1,6 @@
+#ifndef REGEX_TEST_H
+#define REGEX_TEST_H
+
+bool regex_test();
+
+#endif // REGEX_TEST_H
