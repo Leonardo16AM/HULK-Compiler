877fcb03fdaf67ddfa7deca9b16dd8fda8e27dbb
Leonardo16AM <leo16am@gmail.com>
2024-06-17 15:08:42 -0400

    refactor: :rotating_light: Changed from C++ to Python

--- src/tests/automaton_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef AUTOMATON_TEST_H
-#define AUTOMATON_TEST_H
-
-bool automaton_test();
-
-#endif
--- src/tests/backbone_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef BACKBONE_TEST_H
-#define BACKBONE_TEST_H
-
-bool testbackbone();
-
-#endif // BACKBONE_TEST_H
