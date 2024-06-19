d62c9ce325c6b4d23bca67c49ad1fa834c50bc06
Leonardo16AM <leo16am@gmail.com>
2024-06-19 01:43:11 -0400

    feat: :memo: Added regex_tokenizer

---
+++ src/regex/regex_tokenizer.py
@@ -0,0 +1,24 @@
+from cmp.utils import Token
+
+#region regex_tokenizer
+def regex_tokenizer(text, G, skip_whitespaces=True):
+    pipe, star, opar, cpar, symbol, epsilon = G.Terminals('| * ( ) symbol ε')
+    tokens = []
+    fixed_tokens= {
+        '|': pipe,
+        '*': star,
+        '(': opar,
+        ')': cpar,
+        'ε': epsilon
+    }
+
+    for char in text:
+        if skip_whitespaces and char.isspace():
+            continue
+        if char in fixed_tokens:
+            tokens.append(Token(char, fixed_tokens[char]))
+        else:
+            tokens.append(Token(char, symbol))
+
+    tokens.append(Token('$', G.EOF))
+    return tokens
