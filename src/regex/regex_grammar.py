6d69952f05ffa8aca0a421d8041f14798b23536b
Leonardo16AM <leo16am@gmail.com>
2024-06-19 01:34:58 -0400

    Modified Regex Grammar

---
+++ src/regex/regex_grammar.py
@@ -0,0 +1,32 @@
+from cmp.pycompiler import Grammar
+from regex_ast import *
+
+#region regex_grammar
+def regex_grammar():
+    G=Grammar()
+
+    base=G.NonTerminal('<base>',True)
+
+    bext,kleene,batch,union,concat,kext = G.NonTerminals('<bext> <kleene> <batch> <union> <concat> <kext>')
+    pipe,star,oppar,clpar,symb,eps = G.Terminals('| * ( ) symbol ε')
+
+    base %= bext+union,lambda h,s:s[2],None,lambda h,s:s[1]
+
+    union %= pipe+base,lambda h,s:UnionNode(h[0],s[2])
+    union %= G.Epsilon,lambda h,s:h[0]
+
+    bext %= kleene+concat,lambda h,s:s[2],None,lambda h,s:s[1]
+
+    concat %= bext,lambda h,s:ConcatNode(h[0],s[1])
+    concat %= G.Epsilon,lambda h,s:h[0]
+
+    kleene %= batch+kext,lambda h,s:s[2],None,lambda h,s:s[1]
+    
+    kext %= star,lambda h,s:ClosureNode(h[0])
+    kext %= G.Epsilon,lambda h,s:h[0]
+    
+    batch %= symb,lambda h,s:SymbolNode(s[1])
+    batch %= eps,lambda h,s:EpsilonNode(s[1])
+    batch %= oppar+base+clpar,lambda h,s:s[2]
+
+    return G
