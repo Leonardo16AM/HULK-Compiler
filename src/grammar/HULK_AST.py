c9f09bdbc2b7ea940774aa548152dcd208fef13f
EdianBC <edianbc@gmail.com>
2024-06-23 01:05:24 -0400

    Added AST and attributed grammar

---
+++ src/grammar/HULK_AST.py
@@ -0,0 +1,174 @@
+import src.cmp.visitor as visitor
+from src.cmp.ast import *
+from abc import ABC, abstractmethod
+
+class node:
+    pass
+
+
+
+class program_node(node):
+    def __init__(self, dec_list, global_expr):
+        self.dec_list = dec_list
+        self.global_expr = global_expr
+
+class declaration_node(node):
+    pass
+
+class expression_node(node):
+    pass
+
+
+class function_declaration_node(declaration_node):
+    def __init__(self, id, params, return_type, body):
+        self.id = id
+        self.params = params
+        self.return_type = return_type
+        self.body = body
+
+class type_declaration_node(declaration_node):
+    def __init__(self, id, params, features, parent=None):
+        self.id = id
+        self.params = params
+        self.features = features
+        self.parent = parent
+
+class protocol_declaration_node(declaration_node):
+    def __init__(self, id, functions):
+        self.id = id
+        self.functions = functions
+
+class variable_declaration_node(declaration_node):
+    def __init__(self, id, type_id, value):
+        self.id = id
+        self.type_id = type_id
+        self.value = value
+
+class protocol_declaration_node(declaration_node):
+    def __init__(self, id, functions):
+        self.id = id
+        self.functions = functions
+
+class expression_block_node(expression_node):
+    def __init__(self, expressions):
+        self.expressions = expressions
+
+class concatenation_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class and_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class or_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class not_node(expression_node):
+    def __init__(self, expr):
+        self.expr = expr
+
+class is_node(expression_node):
+    def __init__(self, expr, type_id):
+        self.expr = expr
+        self.type_id = type_id
+
+class less_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class less_equal_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class greater_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class greater_equal_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class equals_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class not_equals_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class plus_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class minus_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class multiply_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class divide_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class modulo_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class power_node(expression_node):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+class variable_node(expression_node):
+    def __init__(self, id):
+        self.id = id
+
+class function_call_node(expression_node):
+    def __init__(self, id, args):
+        self.id = id
+        self.args = args
+
+class number_node(expression_node):
+    def __init__(self, value):
+        self.value = value
+
+class bool_node(expression_node):
+    def __init__(self, value):
+        self.value = value
+
+class string_node(expression_node):
+    def __init__(self, value):
+        self.value = value
+
+class index_node(expression_node):
+    def __init__(self, vector, index):
+        self.vector = vector
+        self.index = index
+
+class as_node(expression_node):
+    def __init__(self, expr, type_id):
+        self.expr = expr
+        self.type_id = type_id
+
+class property_call_node(expression_node):
+    def __init__(self, id, calls):
+        self.id = id
+        self.calls = calls
--- src/grammar/HULK_grammar.py
+++ src/grammar/HULK_grammar.py
@@ -7,6 +7,7 @@ from src.cmp.pycompiler import Epsilon
 from src.cmp.pycompiler import Production
 from src.cmp.pycompiler import Grammar
 from src.cmp.utils import pprint, inspect
+from HULK_AST import*
 
 def hulk_grammar():
     
@@ -16,7 +17,7 @@ def hulk_grammar():
     program = G.NonTerminal('program', startSymbol=True)
     dec_list, declaration, function_dec, type_dec, protocol_dec, global_expr = G.NonTerminals('dec_list declaration function_dec type_dec protocol_dec global_expr')
     statement, expr_block, single_expr, statement_list, parameters = G.NonTerminals('statement expr_block single_expr statement_list parameters')
-    parameter, parameter_list, struct_dec_list, struct_dec = G.NonTerminals('parameter parameter_list struct_dec_list struct_dec')
+    parameter, parameter_list, feature_list, feature = G.NonTerminals('parameter parameter_list feature_list feature')
     function_post, function_post_list, parameter_post, parameters_post, parameter_post_list = G.NonTerminals('function_post function_post_list parameter_post parameters_post parameter_post_list')
     single_expr, arithmetic_expr, boolean_expr, string_expr = G.NonTerminals('single_expr arithmetic_expr boolean_expr string_expr')
     boolean_expr_lv2, boolean_expr_lv3, comparation = G.NonTerminals('boolean_expr_lv2 boolean_expr_lv3 comparation')
@@ -34,72 +35,69 @@ def hulk_grammar():
     # endregion
 
     # region Productions
-    program %= dec_list + global_expr
+    program %= dec_list + global_expr, lambda h, s: program_node(s[1], s[2])
 
-    dec_list %= G.Epsilon
-    dec_list %= declaration + dec_list
+    dec_list %= G.Epsilon, lambda h, s: []
+    dec_list %= declaration + dec_list, lambda h, s: [s[1]] + s[2]
 
-    declaration %= function_dec
-    declaration %= type_dec
-    declaration %= protocol_dec
+    declaration %= function_dec, lambda h, s: s[1]
+    declaration %= type_dec, lambda h, s: s[1]
+    declaration %= protocol_dec, lambda h, s: s[1]
 
-    function_dec %= function + id + opar + parameters + cpar + arrow + statement
-    function_dec %= function + id + opar + parameters + cpar + expr_block
+    function_dec %= function + id + opar + parameters + cpar + arrow + statement, lambda h, s: function_declaration_node(s[2], s[4], None, s[7])
+    function_dec %= function + id + opar + parameters + cpar + expr_block, lambda h, s: function_declaration_node(s[2], s[4], None, s[6])
+    function_dec %= function + id + opar + parameters + cpar + colon + type_id + arrow + statement, lambda h, s: function_declaration_node(s[2], s[4], s[7], s[9])
+    function_dec %= function + id + opar + parameters + cpar + colon + type_id + expr_block, lambda h, s: function_declaration_node(s[2], s[4], s[7], s[8])
 
-    parameters %= G.Epsilon
-    parameters %= parameter + parameter_list
+    parameters %= G.Epsilon, lambda h, s: []
+    parameters %= id, lambda h, s: [(s[1], None)]
+    parameters %= id + colon + type_id, lambda h, s: [(s[1], s[3])]
+    parameters %= id + comma + parameters, lambda h, s: [(s[1], None)] + s[3]
+    parameters %= id + colon + type_id + comma + parameters, lambda h, s: [(s[1], s[3])] + s[5]
 
-    parameter_list %= G.Epsilon
-    parameter_list %= comma + parameter + parameter_list
+    # id, params, features, parent
+    type_dec %= type_t + id + lcurly + feature + feature_list + rcurly, lambda h, s: type_declaration_node(s[2], [], [s[4]]+s[5], None)
+    type_dec %= type_t + id + inherits + id + lcurly + feature + feature_list + rcurly, lambda h, s: type_declaration_node(s[2], [], [s[6]]+s[7], s[4])
+    type_dec %= type_t + id + opar + parameters + cpar + lcurly + feature + feature_list + rcurly, lambda h, s: type_declaration_node(s[2], s[4], [s[7]]+s[8], None)
+    type_dec %= type_t + id + inherits + id + opar + parameters + cpar + lcurly + feature + feature_list + rcurly, lambda h, s: type_declaration_node(s[2], s[6], [s[9]]+s[10], s[4])
 
-    parameter %= id 
-    parameter %= id + colon + type_id
+    feature_list %= G.Epsilon, lambda h, s: []
+    feature_list %= feature + feature_list, lambda h, s: [s[1]] + s[2]
 
-    type_dec %= type_t + id + lcurly + struct_dec + struct_dec_list + rcurly
-    type_dec %= type_t + id + inherits + id + lcurly + struct_dec + struct_dec_list + rcurly
+    feature %= id + equal + statement, lambda h, s: variable_declaration_node(s[1], None, s[3])
+    feature %= id + colon + type_id + equal + statement, lambda h, s: variable_declaration_node(s[1], s[3], s[5])
+    feature %= function_dec, lambda h, s: s[1]
 
-    struct_dec_list %= G.Epsilon
-    struct_dec_list %= struct_dec + struct_dec_list
+    protocol_dec %= protocol + id + lcurly + function_post + function_post_list + rcurly, lambda h, s: protocol_declaration_node(s[2], [s[4]]+s[5])
 
-    struct_dec %= id + equal + statement
-    struct_dec %= id + colon + type_id + equal + statement
-    struct_dec %= function_dec
+    function_post_list %= G.Epsilon, lambda h, s: []
+    function_post_list %= function_post + function_post_list, lambda h, s: [s[1]] + s[2]
 
-    protocol_dec %= protocol + id + lcurly + function_post + function_post_list + rcurly
+    function_post %= id + opar + parameters_post + cpar + colon + type_id, lambda h, s: function_declaration_node(s[1], s[3], s[6], None)
 
-    function_post_list %= G.Epsilon
-    function_post_list %= function_post + function_post_list
+    parameters_post %= G.Epsilon, lambda h, s: []
+    parameters_post %= id + colon + type_id, lambda h, s: [(s[1], s[3])]
+    parameters_post %= id + colon + type_id + comma + parameters_post, lambda h, s: [(s[1], s[3])] + s[5]
 
-    function_post %= id + opar + parameters_post + cpar + colon + type_id
+    global_expr %= statement, lambda h, s: s[1]
+    global_expr %= expr_block, lambda h, s: s[1]
 
-    parameters_post %= G.Epsilon
-    parameters_post %= parameter_post + parameter_post_list
+    statement %= single_expr + semicolon, lambda h, s: s[1]
 
-    parameter_post_list %= G.Epsilon
-    parameter_post_list %= comma + parameter_post + parameter_post_list
-    
-    parameter_post %= id + colon + type_id
+    expr_block %= lcurly + statement + statement_list + rcurly, lambda h, s: expression_block_node([s[2]]+s[3])
+    expr_block %= lcurly + expr_block + statement_list + rcurly, lambda h, s: expression_block_node([s[2]]+s[3])
 
-    global_expr %= statement
-    global_expr %= expr_block
+    statement_list %= G.Epsilon, lambda h, s: []
+    statement_list %= statement + statement_list, lambda h, s: [s[1]] + s[2]
+    statement_list %= expr_block + statement_list, lambda h, s: [s[1]] + s[2]
 
-    statement %= single_expr + semicolon
-
-    expr_block %= lcurly + statement + statement_list + rcurly
-    expr_block %= lcurly + expr_block + statement_list + rcurly
-
-    statement_list %= G.Epsilon
-    statement_list %= statement + statement_list
-    statement_list %= expr_block + statement_list
-
-    single_expr %= string_expr
+    single_expr %= string_expr, lambda h, s: s[1]
     single_expr %= if_t + opar + boolean_expr + cpar + single_expr + elif_branch + else_branch
     single_expr %= while_t + opar + boolean_expr + cpar + single_expr
     single_expr %= let + var_list_element + var_list + in_t + single_expr
     single_expr %= for_t + opar + id + in_t + single_expr + cpar + single_expr
     single_expr %= id + assignment_op + single_expr
-    single_expr %= new + type_instance
-    single_expr %= single_expr + as_t + type_id
+    single_expr %= new + type_id + opar + args + cpar
     single_expr %= obracket + args + cbracket
     single_expr %= obracket + single_expr + bar_bar + id + in_t + single_expr + cbracket
 
@@ -114,65 +112,61 @@ def hulk_grammar():
     var_list_element %= id + colon + type_id + equal + single_expr
     var_list_element %= id + equal + single_expr
 
-    string_expr %= boolean_expr
-    string_expr %= string_expr + at + boolean_expr
-    string_expr %= string_expr + double_at + boolean_expr
+    string_expr %= boolean_expr, lambda h, s: s[1]
+    string_expr %= string_expr + at + boolean_expr, lambda h, s: concatenation_node(s[1], s[3])
+    string_expr %= string_expr + double_at + boolean_expr, lambda h, s: concatenation_node(s[1], s[3])
 
-    boolean_expr %= boolean_expr_lv2
-    boolean_expr %= boolean_expr + and_t + boolean_expr_lv2
+    boolean_expr %= boolean_expr_lv2, lambda h, s: s[1]
+    boolean_expr %= boolean_expr + and_t + boolean_expr_lv2, lambda h, s: and_node(s[1], s[3])
 
-    boolean_expr_lv2 %= boolean_expr_lv3
-    boolean_expr_lv2 %= boolean_expr_lv2 + or_t + boolean_expr_lv3
+    boolean_expr_lv2 %= boolean_expr_lv3, lambda h, s: s[1]
+    boolean_expr_lv2 %= boolean_expr_lv2 + or_t + boolean_expr_lv3, lambda h, s: or_node(s[1], s[3])
 
-    boolean_expr_lv3 %= comparation
-    boolean_expr_lv3 %= not_t + comparation
+    boolean_expr_lv3 %= comparation, lambda h, s: s[1]
+    boolean_expr_lv3 %= not_t + comparation, lambda h, s: not_node(s[2])
 
-    comparation %= comparation + is_t + type_id
-    comparation %= arithmetic_expr + less + arithmetic_expr
-    comparation %= arithmetic_expr + greater + arithmetic_expr
-    comparation %= arithmetic_expr + less_eq + arithmetic_expr
-    comparation %= arithmetic_expr + greater_eq + arithmetic_expr
-    comparation %= arithmetic_expr + eq_eq + arithmetic_expr
-    comparation %= arithmetic_expr + dif + arithmetic_expr
-    comparation %= arithmetic_expr
+    comparation %= arithmetic_expr, lambda h, s: s[1]
+    comparation %= comparation + is_t + type_id, lambda h, s: is_node(s[1], s[3])
+    comparation %= arithmetic_expr + less + arithmetic_expr, lambda h, s: less_node(s[1], s[3])
+    comparation %= arithmetic_expr + greater + arithmetic_expr, lambda h, s: greater_node(s[1], s[3])
+    comparation %= arithmetic_expr + less_eq + arithmetic_expr, lambda h, s: less_equal_node(s[1], s[3])
+    comparation %= arithmetic_expr + greater_eq + arithmetic_expr, lambda h, s: greater_equal_node(s[1], s[3])
+    comparation %= arithmetic_expr + eq_eq + arithmetic_expr, lambda h, s: equals_node(s[1], s[3])
+    comparation %= arithmetic_expr + dif + arithmetic_expr, lambda h, s: not_equals_node(s[1], s[3])
 
-    arithmetic_expr %= arithmetic_expr_lv2
-    arithmetic_expr %= arithmetic_expr + plus + arithmetic_expr_lv2
-    arithmetic_expr %= arithmetic_expr + minus + arithmetic_expr_lv2
+    arithmetic_expr %= arithmetic_expr_lv2, lambda h, s: s[1]
+    arithmetic_expr %= arithmetic_expr + plus + arithmetic_expr_lv2, lambda h, s: plus_node(s[1], s[3])
+    arithmetic_expr %= arithmetic_expr + minus + arithmetic_expr_lv2, lambda h, s: minus_node(s[1], s[3])
 
-    arithmetic_expr_lv2 %= arithmetic_expr_lv3
-    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + star + arithmetic_expr_lv3
-    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + div + arithmetic_expr_lv3
-    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + mod + arithmetic_expr_lv3
+    arithmetic_expr_lv2 %= arithmetic_expr_lv3, lambda h, s: s[1]
+    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + star + arithmetic_expr_lv3, lambda h, s: multiply_node(s[1], s[3])
+    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + div + arithmetic_expr_lv3, lambda h, s: divide_node(s[1], s[3])
+    arithmetic_expr_lv2 %= arithmetic_expr_lv2 + mod + arithmetic_expr_lv3, lambda h, s: modulo_node(s[1], s[3])
 
-    arithmetic_expr_lv3 %= arithmetic_expr_lv4
-    arithmetic_expr_lv3 %= arithmetic_expr_lv4 + power + arithmetic_expr_lv3
+    arithmetic_expr_lv3 %= arithmetic_expr_lv4, lambda h, s: s[1]
+    arithmetic_expr_lv3 %= minus + arithmetic_expr_lv3, lambda h, s: minus_node(0, s[2])
 
-    arithmetic_expr_lv4 %= general_atom
-    arithmetic_expr_lv4 %= minus + general_atom
+    arithmetic_expr_lv4 %= general_atom, lambda h, s: s[1]
+    arithmetic_expr_lv4 %= general_atom + power + arithmetic_expr_lv4, lambda h, s: power_node(s[1], s[3])
 
-    general_atom %= id
-    general_atom %= func_call
-    general_atom %= num
-    general_atom %= bool
-    general_atom %= string
-    general_atom %= opar + single_expr + cpar
-    general_atom %= expr_block
-    general_atom %= id + obracket + single_expr + cbracket
-    general_atom %= id + dot + func_call + func_call_list
+    general_atom %= id, lambda h, s: variable_node(s[1])
+    general_atom %= func_call, lambda h, s: s[1]
+    general_atom %= num, lambda h, s: number_node(s[1])
+    general_atom %= bool, lambda h, s: bool_node(s[1])
+    general_atom %= string, lambda h, s: string_node(s[1])
+    general_atom %= opar + single_expr + cpar, lambda h, s: s[2]
+    general_atom %= expr_block, lambda h, s: s[1]
+    general_atom %= id + obracket + single_expr + cbracket, lambda h, s: index_node(s[1], s[3])
+    general_atom %= general_atom + as_t + type_id, lambda h, s: as_node(s[1], s[3])
+    general_atom %= id + dot + func_call + func_call_list, lambda h, s: property_call_node(s[1], [s[3]]+s[4])
 
-    type_instance %= type_id + opar + args + cpar
+    func_call_list %= G.Epsilon, lambda h, s: []
+    func_call_list %= dot + func_call + func_call_list, lambda h, s: [s[2]] + s[3]
 
-    func_call_list %= G.Epsilon
-    func_call_list %= dot + func_call + func_call_list
+    func_call %= id + opar + args + cpar, lambda h, s: function_call_node(s[1], s[3])
 
-    func_call %= id + opar + args + cpar
-
-    args %= G.Epsilon
-    args %= single_expr + single_expr_list
-
-    single_expr_list %= G.Epsilon
-    single_expr_list %= comma + single_expr + single_expr_list
+    args %= G.Epsilon, lambda h, s: []
+    args %= single_expr + comma + args, lambda h, s: [s[1]] + s[3]
     # endregion
 
 G = hulk_grammar()
