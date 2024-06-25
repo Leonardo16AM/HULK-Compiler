5c5082174d97b278c1dde5b4aa86a50fdef803e2
Leonardo16AM <leo16am@gmail.com>
2024-06-25 15:59:16 -0400

    feat: :zap: Enhanced main args

--- src/semantic/semantic_check.py
+++ src/semantic/semantic_check.py
@@ -3,26 +3,26 @@ from src.semantic.type_filler_visitor import type_filler
 from src.semantic.var_finder_visitor import var_finder
 from termcolor import colored
 
-def semantic_check(ast):
+def semantic_check(ast,verbose=False):
     errors=[]
     type_collector = type_finder(errors)
     context,errors=type_collector.visit(ast)
 
     var_collector=var_finder(context,errors)
     scope=var_collector.visit(ast)
-    print(colored(scope,'yellow'))
+    if verbose:print(colored(scope,'yellow'))
 
     type_fill = type_filler(context, errors)
     context,errors=type_fill.visit(ast)
 
 
-    print(context)
+    if verbose:print(context)
 
     if errors:
         for error in errors:
             print(error)
         return False
     
-    print("NO SEMANTIC ERRORS FOUND")
+    if verbose:print("NO SEMANTIC ERRORS FOUND")
     return True
 
